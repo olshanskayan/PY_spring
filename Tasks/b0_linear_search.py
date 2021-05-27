e509e7f43e0d97d0463b8327b8fc244ccd0859a1
Наталья <olshanskaya@mail.ru>
2021-05-27 19:01:00 +0300

    binary search done

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -3,6 +3,7 @@ This module implements some functions based on linear search algo
 """
 from typing import Sequence
 
+my_array = [44, 20, 11, 31]
 
 def min_search(arr: Sequence) -> int:
     """
@@ -11,5 +12,17 @@ def min_search(arr: Sequence) -> int:
     :param arr: Array containing numbers
     :return: index of first occurrence of minimal element in array
     """
-    print(arr)
+    min_index = 0
+    min_value = my_array[min_index]
+
+    for index, value in enumerate(my_array):
+        if value < min_value:
+            min_value = value
+
+
+
+    print(min_value)
     return -1
+
+if __name__ == '__main__':
+    min_search(my_array)
