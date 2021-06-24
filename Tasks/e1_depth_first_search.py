ecdd3a21357191c8e55412c95557f2a0bea5ec99
Наталья <olshanskaya@mail.ru>
2021-06-24 20:39:10 +0300

    steck

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -1,5 +1,6 @@
 from typing import Hashable, List
 import networkx as nx
+from collections import deque
 
 
 def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +11,37 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    visited = {node: False for node in g.nodes}
+    path = []
+
+    # # d.append() #добавить в конец
+    # # d.popleft() #забрать сначала
+    #
+    # d.append(start_node)
+    # visited[start_node] = True
+    # while d:
+    #     #        current_node = d.popleft()
+    #     current_node = d.pop()
+    #     path.append(current_node)
+    #
+    #     for neighbor in g.neighbors(current_node):
+    #         if not visited[neighbor]:
+    #             d.append(neighbor)  # поджигаем узел графа
+    #             visited[neighbor] = True  # если узел подожжен, то мы его посещали
+
+    def recursion_dfs(current_node: Hashable):
+
+        visited[current_node] = True
+        path.append(current_node)
+
+        for neighbor in g.neighbors(current_node):
+            if not visited[neighbor]:
+                # visited[neighbor] = True  # если узел подожжен, то мы его посещали
+                recursion_dfs(neighbor)
+        return path
+
+    recursion_dfs(start_node)
+    return path
+    # print(g, start_node)
+    # return list(g.nodes)
+
